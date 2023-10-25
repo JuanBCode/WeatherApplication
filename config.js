30cec5de462a35a61e7719c721c0bd0e06470570
JuanBCode <143307530+JuanBCode@users.noreply.github.com>
2023-10-25 10:17:45 -0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-//This file will be added to .gitignore - which is not working right now with proper env setup
-//but will still use it to learn about export/import
-
-//apikey for openweathermap API access
-
-export const apiKey = "5f34487ddc5ca2920758541d467ce417";
